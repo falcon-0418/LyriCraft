cf723bdf2abae720181434d9c7f21ee676ad0591
PIKO <shinsedaikakumei@gmail.com>
2024-01-12 12:58:44 +0900

    Revert "ヘッダーとフッターとTOPページ作成とエディターとのルーティング"
    This reverts commit af6fc875a0059d3d1202538e943270239329e9d8.

--- front_end/app/page.tsx
+++ front_end/app/page.tsx
@@ -1,19 +1,9 @@
-import Link from "next/link";
-import SharedLayout from './layout/sharedLayout';
+import dynamic from 'next/dynamic';
 
-export default function Home() {
-  return (
-    <SharedLayout>
-      <main>
-        <div className="flex flex-col h-screen justify-center items-center">
-          <div className="text-center">
-            <h2 className="text-6xl font-bold mb-8">LyriCraft</h2>
-              <Link href="page/Editor">
-                始める
-              </Link>
-          </div>
-        </div>
-      </main>
-    </SharedLayout>
-  );
+const MainEditor = dynamic(() => import('./components/Editor/mainEditor'), {
+  ssr: false,
+});
+
+export default function EditorPage() {
+  return <MainEditor />;
 }
